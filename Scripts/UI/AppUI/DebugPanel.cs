b3b8d6d7807fbc6fcd2d0d8233af67abe308dd23
Marcin Świtała <marcin.switala.wwa@gmail.com>
2019-11-21 23:26:57 +0100

    -lerp fps in DebugPanel

--- Scripts/UI/AppUI/DebugPanel.cs
+++ Scripts/UI/AppUI/DebugPanel.cs
@@ -5,39 +5,26 @@ namespace MustHave.UI
 {
     public class DebugPanel : UIScript
     {
-        [SerializeField] private Text fpsText = default;
-        [SerializeField] private Text debugText = default;
+        [SerializeField] private Text _fpsText = default;
+        [SerializeField] private Text _debugText = default;
 
-        private float fpsCounter;
-        private float fpsAvgStartTime;
+        private float _fps = default;
 
-        public string DebugText { set { debugText.text = value; } }
+        public string DebugText { set { _debugText.text = value; } }
 
         public void SetDebugText(string text)
         {
-            debugText.text = text;
-        }
-
-        protected override void Start()
-        {
-            fpsCounter = 0;
-            fpsAvgStartTime = Time.realtimeSinceStartup;
+            _debugText.text = text;
         }
 
         private void Update()
         {
-            float deltaTime = Time.unscaledDeltaTime > 0.0001f ? Time.unscaledDeltaTime : 1f;
-            float fps = 1f / deltaTime;
-            deltaTime = Time.realtimeSinceStartup - fpsAvgStartTime;
-            deltaTime = deltaTime > 0.0001f ? deltaTime : 1f;
-            fpsCounter++;
-            float fpsAvg = fpsCounter / deltaTime;
-            if (deltaTime >= 3f)
+            float unscaledDeltaTime = Time.unscaledDeltaTime;
+            if (unscaledDeltaTime > 0f)
             {
-                fpsAvgStartTime = Time.realtimeSinceStartup;
-                fpsCounter = 0;
+                _fps = Mathf.Lerp(_fps, 1f / unscaledDeltaTime, 2f * unscaledDeltaTime);
+                _fpsText.text = "FPS: " + _fps.ToString("F2");
             }
-            fpsText.text = string.Concat("fps=", fps.ToString("n2"), "\n", "fpsAvg=", fpsAvg.ToString("n2"));
         }
     }
 }
