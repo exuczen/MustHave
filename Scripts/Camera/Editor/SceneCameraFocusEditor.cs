787cec350936d3f22329ccb356e59645947f8a8c
Marcin Świtała <marcin.switala.wwa@gmail.com>
2020-12-05 11:40:26 +0100

    Added SceneCameraFocus.

---
+++ Scripts/Camera/Editor/SceneCameraFocusEditor.cs
@@ -0,0 +1,39 @@
+﻿using MustHave;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace MustHave
+{
+    [CustomEditor(typeof(SceneCameraFocus))]
+    public class SceneCameraFocusEditor : Editor
+    {
+        private void OnSceneGUI()
+        {
+            Event currEvent = Event.current;
+            if (currEvent.type == EventType.KeyDown && currEvent.keyCode == KeyCode.F)
+            {
+                SceneCameraFocus cameraFocus = target as SceneCameraFocus;
+                if (cameraFocus.transform is RectTransform)
+                {
+                    cameraFocus.AlignToForwardAxis();
+                }
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            SceneCameraFocus cameraFocus = target as SceneCameraFocus;
+            GUILayout.BeginHorizontal("Box");
+            GUILayout.Label("Forward Axis Sign: ");
+            int selectedFwdAxisSignIndex = (cameraFocus.ForwardAxisSign + 1) >> 1;
+            selectedFwdAxisSignIndex = GUILayout.SelectionGrid(selectedFwdAxisSignIndex, new string[] { " - ", " + " }, 2);
+            cameraFocus.ForwardAxisSign = (selectedFwdAxisSignIndex << 1) - 1;
+            GUILayout.EndHorizontal();
+            
+        }
+    }
+}
---
+++ Scripts/Camera/SceneCameraFocus.cs
@@ -0,0 +1,30 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace MustHave
+{
+    [ExecuteInEditMode]
+    public class SceneCameraFocus : MonoBehaviour
+    {
+        [SerializeField, HideInInspector] private int forwardAxisSign = 1;
+
+        public int ForwardAxisSign { get { return Math.Sign(forwardAxisSign); } set { forwardAxisSign = Math.Sign(value); } }
+
+        public void AlignToForwardAxis()
+        {
+            SceneView sceneView = SceneView.lastActiveSceneView;
+            if (sceneView)
+            {
+                Vector3 forward = -ForwardAxisSign * transform.forward;
+                sceneView.pivot = transform.position;
+                sceneView.rotation = Quaternion.LookRotation(forward, transform.up);
+                sceneView.Repaint();
+            }
+        }
+    }
+}
+#endif
