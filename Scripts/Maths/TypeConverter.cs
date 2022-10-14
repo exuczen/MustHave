d40b482ec06c559c06aee703945198c6c5570ef9
Marcin Świtała <marcin.switala.wwa@gmail.com>
2022-10-14 21:27:32 +0200

    Shortened method names in TypoeConverter.

--- Scripts/Maths/TypeConverter.cs
+++ Scripts/Maths/TypeConverter.cs
@@ -4,13 +4,13 @@ namespace MustHave
 {
     public static class TypeConverter
     {
-        public static short ConvertFloatToShort(float value, float max)
+        public static short FloatToShort(float value, float max)
         {
             value = Mathf.Clamp01(value / max) * short.MaxValue;
             return (short)value;
         }
 
-        public static float ConvertShortToFloat(short value, float max)
+        public static float ShortToFloat(short value, float max)
         {
             return value * max / short.MaxValue;
         }
