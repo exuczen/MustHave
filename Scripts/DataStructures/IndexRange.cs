9512bc3878fbcb32024acb16bc84a27d3329e26b
Marcin Świtała <marcin.switala.wwa@gmail.com>
2023-06-24 20:07:21 +0200

    Added ArrayUtils, IndexRange.

---
+++ Scripts/DataStructures/IndexRange.cs
@@ -0,0 +1,62 @@
+﻿namespace MustHave
+{
+    public struct IndexRange
+    {
+        public static readonly IndexRange None = new IndexRange(-1);
+
+        public int FullLength { get; private set; }
+
+        public int Beg;
+        public int End;
+
+        public static int GetNext(int index, int length) => (index + 1) % length;
+
+        public static int GetPrev(int index, int length) => (index - 1 + length) % length;
+
+        public static int GetIndexCount(int beg, int end, int length)
+        {
+            if (length < 0)
+            {
+                return -1;
+            }
+            else if (end >= beg)
+            {
+                return end - beg + 1;
+            }
+            else
+            {
+                return length - (beg - end) + 1;
+            }
+        }
+
+        public IndexRange(int fullLength)
+        {
+            FullLength = fullLength;
+            Beg = -1;
+            End = -1;
+        }
+
+        public IndexRange(int beg, int end, int fullLength)
+        {
+            Beg = beg;
+            End = end;
+            FullLength = fullLength;
+        }
+
+        public int GetIndexCount() => GetIndexCount(Beg, End, FullLength);
+
+        public int GetNext(int index) => GetNext(index, FullLength);
+
+        public int GetPrev(int index) => GetPrev(index, FullLength);
+
+        public IndexRange GetInverseRange()
+        {
+            return new IndexRange(GetNext(End), GetPrev(Beg), FullLength);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("IndexRange: {0} - {1} | {2}", Beg, End, FullLength);
+        }
+    }
+}
---
+++ Scripts/Utils/ArrayUtils.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace MustHave
+{
+    public struct ArrayUtils
+    {
+        public static int CutRange<T>(T[] srcArray, IndexRange range, T[] destArray)
+        {
+            int length = range.FullLength;
+            int count = length - range.GetIndexCount();
+            if (count <= 0)
+            {
+                return 0;
+            }
+            int beg = range.Beg;
+            int end = range.End;
+            int last = length - 1;
+
+            if (end < beg || beg == 0)
+            {
+                Array.Copy(srcArray, end + 1, destArray, 0, count);
+            }
+            else if (end < last)
+            {
+                Array.Copy(srcArray, end + 1, destArray, 0, last - end);
+                Array.Copy(srcArray, 0, destArray, last - end, beg);
+            }
+            else
+            {
+                Array.Copy(srcArray, 0, destArray, 0, beg);
+            }
+            return count;
+        }
+
+        public static void Swap<T>(T[] array, int a, int b)
+        {
+            (array[b], array[a]) = (array[a], array[b]);
+        }
+
+        public static void InvertOrder<T>(T[] array, int count)
+        {
+            int halfCount = count >> 1;
+            for (int i = 0; i < halfCount; i++)
+            {
+                Swap(array, i, count - 1 - i);
+            }
+        }
+
+        public static void InvertOrder<T>(T[] array, int beg, int end)
+        {
+            int count = end - beg + 1;
+            int halfCount = count >> 1;
+            for (int i = 0; i < halfCount; i++)
+            {
+                Swap(array, beg + i, end - i);
+            }
+        }
+    }
+}
