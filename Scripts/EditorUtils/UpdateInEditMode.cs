d82a0a37e126ce0445bf86ade71c899676c6c35f
Marcin Świtała <marcin.switala.wwa@gmail.com>
2024-03-09 01:17:53 +0100

    Added UpdateInEditMode.

---
+++ Scripts/EditorUtils/UpdateInEditMode.cs
@@ -0,0 +1,37 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+namespace MustHave
+{
+    [ExecuteInEditMode]
+    public class UpdateInEditMode : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        private void OnEnable()
+        {
+            if (!EditorApplication.isPlaying)
+            {
+                EditorApplication.update += OnEditorUpdate;
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (!EditorApplication.isPlaying)
+            {
+                EditorApplication.update -= OnEditorUpdate;
+            }
+        }
+
+        private void OnEditorUpdate()
+        {
+            if (!EditorApplication.isPlaying)
+            {
+                EditorApplication.QueuePlayerLoopUpdate();
+            }
+        }
+#endif
+    }
+}
