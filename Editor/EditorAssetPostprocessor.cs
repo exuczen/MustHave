636e450fe607f16b0c9a289b16a7c09589f3b42e
Marcin Świtała <marcin.switala.wwa@gmail.com>
2024-05-08 00:00:33 +0200

    Added EditorAssetPostprocessor.

---
+++ Editor/EditorAssetPostprocessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEditor;
+
+namespace MustHave
+{
+    public class EditorAssetPostprocessor : AssetPostprocessor
+    {
+        public static event Action AllAssetsPostprocessed = delegate { };
+
+        private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            AllAssetsPostprocessed();
+        }
+    }
+}
