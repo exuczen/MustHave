2585bdaaa48e072446e79e7d4d496a79639e705b
Marcin Świtała <marcin.switala.wwa@gmail.com>
2024-05-15 23:19:27 +0200

    Added ColoExtensionMethods with ToRGBA. Added Color32FromRGBA to ColorUtils.

--- Scripts/Utils/ColorUtils.cs
+++ Scripts/Utils/ColorUtils.cs
@@ -3,8 +3,39 @@ using System.Collections;
 
 namespace MustHave.Utils
 {
+    public static class ColorExtensionMethods
+    {
+        public static int ToRGBA(this Color color)
+        {
+            Color32 color32 = color;
+            return color32.ToRGBA();
+        }
+
+        public static int ToRGBA(this Color32 color)
+        {
+            int a = color.a << 0;
+            int b = color.b << 8;
+            int g = color.g << 16;
+            int r = color.r << 24;
+            int rgba = r | g | b | a;
+            //Debug.Log($"From color: {ColorUtility.ToHtmlStringRGBA(color)} to RGBA: {rgba:X8}");
+            return rgba;
+        }
+    }
+
     public struct ColorUtils
     {
+        public static Color32 Color32FromRGBA(int rgba)
+        {
+            //int debugRGBA = rgba;
+            byte a = (byte)(rgba >>= 0 & 0xff);
+            byte b = (byte)(rgba >>= 8 & 0xff);
+            byte g = (byte)(rgba >>= 8 & 0xff);
+            byte r = (byte)(rgba >>= 8 & 0xff);
+            //Debug.Log($"From RGBA: {debugRGBA:X8} to color: {ColorUtility.ToHtmlStringRGBA(new Color32(r, g, b, a))}");
+            return new Color32(r, g, b, a);
+        }
+
         public static Color32 Color32FromARGB(int argb)
         {
             byte b = (byte)(argb >>= 0 & 0xff);
