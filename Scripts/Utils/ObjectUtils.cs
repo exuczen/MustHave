682ddf6b5ab7abb729ef7820eeead114ac320681
Marcin Świtała <marcin.switala.wwa@gmail.com>
2024-05-17 21:00:27 +0200

    Added ObjectUtils.

---
+++ Scripts/Utils/ObjectUtils.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+
+namespace MustHave
+{
+    public struct ObjectUtils
+    {
+        public static void DestroyComponent<T>(ref T component) where T : Component
+        {
+            DestroyComponentOrGameObject(ref component, c => c);
+        }
+
+        public static void DestroyGameObject<T>(ref T component) where T : Component
+        {
+            DestroyComponentOrGameObject(ref component, c => c.gameObject);
+        }
+
+        public static void DestroyComponentOrGameObject<T>(ref T component, Func<T, Object> getObject) where T : Component
+        {
+            if (component && component.gameObject)
+            {
+                Destroy(getObject(component));
+            }
+            component = null;
+        }
+
+        public static void Destroy(Object obj)
+        {
+            if (obj)
+            {
+                if (Application.isPlaying)
+                {
+                    Object.Destroy(obj);
+                }
+                else
+                {
+                    var activeScene = SceneManager.GetActiveScene();
+
+                    if (activeScene.isLoaded && activeScene.IsValid())
+                    {
+                        Object.DestroyImmediate(obj);
+                    }
+                }
+            }
+        }
+    }
+}
