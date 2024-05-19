ba01be601e5be50edb64ad843e3959c490e2290c
Marcin Świtała <marcin.switala.wwa@gmail.com>
2024-05-19 18:17:01 +0200

    Added ShaderUtils.

---
+++ Scripts/Utils/ShaderUtils.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace MustHave.Utils
+{
+    public struct ShaderUtils
+    {
+        public static void PrintComputeShaderKeywords(ComputeShader shader)
+        {
+            Debug.Log($"{shader.name}: keywordSpace.keywordCount: {shader.keywordSpace.keywordCount}");
+            int i = 0;
+            foreach (var kw in shader.keywordSpace.keywords)
+            {
+                Debug.Log($"{shader.name}: keywordSpace.keywords[{i++}]: {kw}");
+            }
+            i = 0;
+            foreach (var kw in shader.shaderKeywords)
+            {
+                Debug.Log($"{shader.name}: shaderKeywords[{i++}]: {kw}");
+            }
+            i = 0;
+            foreach (var kw in shader.enabledKeywords)
+            {
+                Debug.Log($"{shader.name}: enabledKeywords[{i++}]: {kw}");
+            }
+        }
+    }
+}
