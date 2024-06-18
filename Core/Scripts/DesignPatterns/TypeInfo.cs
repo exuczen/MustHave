6bdb2f894d87376af55a4eefb3db903548077078
Marcin Świtała <marcin.switala.wwa@gmail.com>
2024-06-18 21:49:16 +0200

    Added generic TypeInfo. Added HDRPAssetInfo. Changed HDRPAssetConfiguration to HDRPAssetExtensionMethods.

---
+++ Core/Scripts/DesignPatterns/TypeInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace MustHave
+{
+    public class TypeInfo<T>
+    {
+        public const BindingFlags InternalFieldFlags = BindingFlags.Instance | BindingFlags.Default | BindingFlags.Public | BindingFlags.NonPublic;
+
+        public static readonly Type Type = typeof(T);
+
+        public static FieldInfo GetFieldInfo(string name, BindingFlags flags = InternalFieldFlags) => Type.GetField(name, flags);
+    }
+}
---
+++ Shared/Scripts/Rendering/HDRP/HDRPAssetExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_PIPELINE_HDRP
+using System.Reflection;
+using UnityEngine.Rendering.HighDefinition;
+
+namespace MustHave
+{
+    public class HDRPAssetInfo : TypeInfo<HDRenderPipelineAsset>
+    {
+        public static readonly FieldInfo m_RenderPipelineSettings = GetFieldInfo("m_RenderPipelineSettings");
+    }
+
+    public static class HDRPAssetExtensionMethods
+    {
+        public static void ModifySettings(this HDRenderPipelineAsset asset, ActionRef<RenderPipelineSettings> modify)
+        {
+            var settings = asset.currentPlatformRenderPipelineSettings;
+            modify(ref settings);
+            HDRPAssetInfo.m_RenderPipelineSettings.SetValue(asset, settings);
+#if UNITY_EDITOR
+            AssetUtils.SaveAsset(asset);
+#endif
+        }
+    }
+}
+#endif
